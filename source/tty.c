018aaa63064b66f2e1ff9a757f7d61e1ae6b6eb7
Charles Childers <crc.platypus@gmail.com>
2009-08-19 19:46:34 -0400

    tty interface is clean now

--- source/tty.c
+++ source/tty.c
@@ -3,15 +3,13 @@
  *
  * This code is gifted to the public domain. Share freely.
  ************************************************************************/
+
 /*
- * 
- tty interface routines
- this is used by those systems that have a bare
- * tty interface
- systems using another interface, such as the stdwin
- * interface
- will replace this file with another.
- */ 
+ * tty interface routines
+ * this is used by those systems that have a bare
+ * tty interface systems using another interface, such as the stdwin
+ * interface  will replace this file with another.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,85 +20,55 @@
 extern boolean  parseok;
 
 
- /* report a fatal system error */ 
-noreturn sysError(char *s1, char *s2) 
+/* report a fatal system error */
+noreturn sysError(char *s1, char *s2)
 {
-  
-    ignore fprintf(stderr, "%s\n%s\n", s1, s2);
-  
-    ignore abort();
-  
-} 
+  ignore fprintf(stderr, "%s\n%s\n", s1, s2);
+  ignore abort();
+}
 
- /* report a nonfatal system error */ 
-noreturn sysWarn(char *s1, char *s2) 
+/* report a nonfatal system error */
+noreturn sysWarn(char *s1, char *s2)
 {
-  
-    ignore fprintf(stderr, "%s\n%s\n", s1, s2);
-  
-} 
+  ignore fprintf(stderr, "%s\n%s\n", s1, s2);
+}
 
-void 
-compilWarn(char *selector, char *str1, char *str2) 
+void compilWarn(char *selector, char *str1, char *str2)
 {
-  
-  ignore fprintf(stderr, "compiler warning: Method %s : %s %s\n", 
-		 selector, str1, str2);
-  
-} 
+  ignore fprintf(stderr, "compiler warning: Method %s : %s %s\n", selector, str1, str2);
+}
 
-void 
-compilError(char *selector, char *str1, char *str2) 
+void compilError(char *selector, char *str1, char *str2)
 {
-  
-  ignore fprintf(stderr, "compiler error: Method %s : %s %s\n", 
-		 selector, str1, str2);
-  
-    parseok = false;
-  
-} 
+  ignore fprintf(stderr, "compiler error: Method %s : %s %s\n", selector, str1, str2);
+  parseok = false;
+}
 
-noreturn dspMethod(char *cp, char *mp) 
+noreturn dspMethod(char *cp, char *mp)
 {
-  
-     /* ignore fprintf(stderr,"%s %s\n", cp, mp); */ 
-} 
+  /* ignore fprintf(stderr,"%s %s\n", cp, mp); */
+}
 
-void 
-givepause() 
+void givepause()
 {
-  
-  char            buffer[80];
-  
-    
-    ignore fprintf(stderr, "push return to continue\n");
-  
-    ignore fgets(buffer, 80, stdin);
-  
-} 
+  char buffer[80];
+  ignore fprintf(stderr, "push return to continue\n");
+  ignore fgets(buffer, 80, stdin);
+}
 
-object sysPrimitive(int number, object * arguments) 
+object sysPrimitive(int number, object * arguments)
 {
-  
-    object returnedObject;
-  
-    
-     /* someday there will be more here */ 
-    switch (number - 150)
+  object returnedObject;
+
+  /* someday there will be more here */
+  switch (number - 150)
   {
-    
-  case 0:			/* do a system() call */
-    
+    /* do a system() call */
+    case 0:
       returnedObject = newInteger(system(charPtr(arguments[0])));
-    
       break;
-    
-      
-  default:
-    
+    default:
       sysError("unknown primitive", "sysPrimitive");
-    
-  } 
-    return (returnedObject);
-  
-} 
+  }
+  return (returnedObject);
+}
